d38f99225804d377cc8b5a4da1b1f059fa731929
rainkolwa <pluvo.lee@gmail.com>
2019-06-21 00:23:35 +0800

    test: mainfest

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,5 +24,19 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `清风徐来`,
+        short_name: `清风`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
   ],
 }
