80df26522f2f9c3b25503586c0b5c325f7378ed7
rainkolwa <pluvo.lee@gmail.com>
2019-06-20 13:33:02 +0800

    test: markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,5 +23,6 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    `gatsby-transformer-remark`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,50 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import styles from "../styles/postitem.module.css"
 
-export default ({ data }) => (
-  <Layout>
-    <div>Hello world? </div>
-  </Layout>
-)
+const PostItem = ({ data }) => {
+  return (
+    <li className={styles.item}>
+      <h3>{data.frontmatter.title}</h3>
+      <p>{data.excerpt}</p>
+      <div>{data.frontmatter.date}</div>
+    </li>
+  )
+}
+
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <ul className={styles.container}>
+        {data.allMarkdownRemark.nodes.map(post => (
+          <PostItem key={post.id} data={post}></PostItem>
+        ))}
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
+      pageInfo {
+        hasNextPage
+        currentPage
+        hasPreviousPage
+        itemCount
+        pageCount
+        perPage
+      }
+      nodes {
+        excerpt(truncate: true, pruneLength: 100)
+        id
+        frontmatter {
+          title
+          date(formatString: "YY/MM/DD HH:mm")
+        }
+      }
+    }
+  }
+`
