351f9c71f2238d61faa7d753f699fc6783b9091f
rainkolwa <pluvo.lee@gmail.com>
2019-07-15 14:31:03 +0800

    ga

--- src/html.js
+++ src/html.js
@@ -12,6 +12,17 @@ export default function HTML (props) {
           content='width=device-width, initial-scale=1, shrink-to-fit=no'
         />
         {props.headComponents}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5V45GJT');
+                `
+          }}
+        />
         <script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' />
         <script
           dangerouslySetInnerHTML={{
@@ -26,6 +37,10 @@ export default function HTML (props) {
         />
       </head>
       <body {...props.bodyAttributes}>
+        <noscript>
+          <iframe src='https://www.googletagmanager.com/ns.html?id=GTM-5V45GJT'
+            height='0' width='0' style={{ display: 'none', visibility: 'hidden' }} />
+        </noscript>
         {props.preBodyComponents}
         <noscript key='noscript' id='gatsby-noscript'>
           This app works best with JavaScript enabled.
