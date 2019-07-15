28f011590217a0564fcb2d01500497a76c659c2a
rainkolwa <pluvo.lee@gmail.com>
2019-07-15 14:43:52 +0800

    ga

--- src/html.js
+++ src/html.js
@@ -12,15 +12,14 @@ export default function HTML (props) {
           content='width=device-width, initial-scale=1, shrink-to-fit=no'
         />
         {props.headComponents}
+        <script async src='https://www.googletagmanager.com/gtag/js?id=UA-47040019-1' />
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5V45GJT');
-                `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-47040019-1');`
           }}
         />
         <script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' />
@@ -37,10 +36,6 @@ export default function HTML (props) {
         />
       </head>
       <body {...props.bodyAttributes}>
-        <noscript>
-          <iframe src='https://www.googletagmanager.com/ns.html?id=GTM-5V45GJT'
-            height='0' width='0' style={{ display: 'none', visibility: 'hidden' }} />
-        </noscript>
         {props.preBodyComponents}
         <noscript key='noscript' id='gatsby-noscript'>
           This app works best with JavaScript enabled.
