07f1374c0f9e9030753114c68bbcf3b1292a0a46
rainkolwa <rainkolwa@gmail.com>
2021-11-22 16:30:28 +0800

    fix: baidu

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,10 @@ import './src/styles/theme.css'
 import './src/styles/global.styl'
 import 'prismjs/themes/prism-tomorrow.css'
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
+
+exports.onRouteUpdate = function({ location }) {
+  // Don't track while developing.
+  if (process.env.NODE_ENV === `production` && typeof _hmt === `function`) {
+    _hmt.push(['_trackPageview', (location || {}).pathname])
+  }
+}
--- src/html.js
+++ src/html.js
@@ -2,35 +2,41 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default function HTML (props) {
+  const siteid = '81431c4d1dbcd9b15cef163e474857d7'
+  const tongji = (
+    <script
+      key={`baidu-tongji`}
+      dangerouslySetInnerHTML={{
+        __html: `
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?${siteid}";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+`,
+      }}
+    />
+  )
   return (
     <html {...props.htmlAttributes}>
       <head>
-        <meta charSet='utf-8' />
-        <meta httpEquiv='x-ua-compatible' content='ie=edge' />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        {props.headComponents}
-        <script>
-        var _hmt = _hmt || [];
-        (function() {
-          var hm = document.createElement("script");
-          hm.src = "https://hm.baidu.com/hm.js?81431c4d1dbcd9b15cef163e474857d7";
-          var s = document.getElementsByTagName("script")[0]; 
-          s.parentNode.insertBefore(hm, s);
-        })();
-        </script>
-
+        {[...props.headComponents, tongji]}
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
-        <noscript key='noscript' id='gatsby-noscript'>
+        <noscript key="noscript" id="gatsby-noscript">
           This app works best with JavaScript enabled.
         </noscript>
         <div
           key={`body`}
-          id='___gatsby'
+          id="___gatsby"
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
