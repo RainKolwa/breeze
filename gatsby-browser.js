257f14b5bde92663bbd44a98d00a550b919853f0
rainkolwa <rainkolwa@gmail.com>
2025-11-24 22:55:17 +0800

    chore: build

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,37 @@ require('./src/styles/global.styl')
 require('prismjs/themes/prism-tomorrow.css')
 require('prismjs/plugins/line-numbers/prism-line-numbers.css')
 
+const ensureLegacyChunkNames = () => {
+  if (typeof window === 'undefined' || !window.___chunkMapping) {
+    return
+  }
+
+  const suffixes = [
+    [/[-]tsx$/, '-js'],
+    [/[-]ts$/, '-js'],
+  ]
+
+  Object.keys(window.___chunkMapping).forEach(chunkName => {
+    suffixes.forEach(([pattern, replacement]) => {
+      if (!pattern.test(chunkName)) {
+        return
+      }
+
+      const legacyName = chunkName.replace(pattern, replacement)
+      if (!window.___chunkMapping[legacyName]) {
+        window.___chunkMapping[legacyName] = window.___chunkMapping[chunkName]
+      }
+    })
+  })
+}
+
+exports.onClientEntry = () => {
+  ensureLegacyChunkNames()
+}
+
 exports.onRouteUpdate = ({ location }) => {
+  ensureLegacyChunkNames()
+
   if (
     process.env.NODE_ENV === `production` &&
     typeof window._hmt === `function`
